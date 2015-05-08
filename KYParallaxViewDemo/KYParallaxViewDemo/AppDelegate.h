3806f7ade47f16fd3abc6ed1707ada63c3657c63
KittenYang <kittenyang@icloud.com>
2015-05-09 00:51:55 +0800

    first commit

---
+++ KYParallaxViewDemo/KYParallaxViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYParallaxViewDemo
+//
+//  Created by Kitten Yang on 5/9/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
